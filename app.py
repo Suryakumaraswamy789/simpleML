d287b80988aa23ae0a60784eff1045973a1f59db
Suryakumaraswamy789 <pandisurya455@gmail.com>
2025-09-02 15:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ education=st.sidebar.selectbox("Education Level",['Bachelor','Masters','Phd','HS
 occupation=st.sidebar.selectbox("JobRole",{'Tech-Support','Craft-repair','Other-service',"Sales","Exec-managerial", "Prof-specialty", "Handlers-cleaners", "Machine-op-inspct","Adm-clerical", "Farming-fishing", "Transport-moving", "Priv-house-serv","Protective-serv", "Armed-Forces"])
 hours_per_week=st.sidebar.slider("Hours per weeek",1,86,40)
 experience = st.sidebar.slider("Years of Experience:", 0, 40, 5)
-
 # Build input DataFrame (must match preprocessing of your training data)
 input_df = pd.DataFrame({
     'age': [age],
